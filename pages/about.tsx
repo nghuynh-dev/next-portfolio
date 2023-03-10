0730d7bfbf524d4eb16f419cba8c574cd0cc5eb4
Huynh Nguyen <nghuynh.dev@gmail.com>
2023-03-10 11:59:26 +0700

    Setup routing page

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import { MainLayout } from '@/components/layout'
+import * as React from 'react'
+
+export interface AboutPageProps {}
+
+export default function AboutPage(props: AboutPageProps) {
+  return (
+    <>
+      <div>ABOUT PAGE</div>
+    </>
+  )
+}
+AboutPage.Layout = MainLayout
