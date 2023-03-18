3b2e4ba6a203d0969f9a0ea9ea139fe8cdfc72a2
Huynh Nguyen <nghuynh.dev@gmail.com>
2023-03-18 22:17:14 +0700

    Setup MUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,29 @@
 import { AppPropsWithLayout } from '@/models'
 import '@/styles/globals.css'
+import createEmotionCache from '@/utils/create-emotion-cache'
+import theme from '@/utils/theme'
+import { CacheProvider } from '@emotion/react'
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
 import { EmptyLayout } from './../components/layout/empty'
 
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache()
+
+export default function App({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}: AppPropsWithLayout) {
   const Layout = Component.Layout ?? EmptyLayout
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </CacheProvider>
   )
 }
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,13 +1,16 @@
 import { MainLayout } from '@/components/layout'
 import * as React from 'react'
+import { Box, Typography } from '@mui/material'
 
 export interface AboutPageProps {}
 
 export default function AboutPage(props: AboutPageProps) {
   return (
-    <>
-      <div>ABOUT PAGE</div>
-    </>
+    <Box>
+      <Typography component="h1" variant="h3" color="primary">
+        ABOUT PAGE
+      </Typography>
+    </Box>
   )
 }
 AboutPage.Layout = MainLayout
